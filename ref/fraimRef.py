35fc4b3e3cab94e92fc79fbfcbdb8ab084de9376
double-o-f <83484832+double-o-f@users.noreply.github.com>
2022-08-20 16:58:26 -0500

    circle

--- ref/fraimRef.py
+++ ref/fraimRef.py
@@ -18,10 +18,6 @@ screen = pygame.display.set_mode([rezH*rezMul, rezV*rezMul])
 running = True
 while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
     screen.fill((255, 255, 255))
 
     pygame.draw.circle(screen, (0, 0, 255), ((rezH/2)*rezMul, (rezV/2)*rezMul), rezMul)
